3e4f0fefad9f8a79366200247c3986033e61c392
emirkantul <94080241+emirkantul@users.noreply.github.com>
2021-12-27 04:02:58 +0300

    minor heroku
    Co-Authored-By: ipekteselli <94112863+ipekteselli@users.noreply.github.com>
    Co-Authored-By: larapole <87978658+larapole@users.noreply.github.com>

--- storefront/settings.py
+++ storefront/settings.py
@@ -140,7 +140,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -157,3 +157,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
